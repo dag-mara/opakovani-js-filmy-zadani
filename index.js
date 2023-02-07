ec5dfd5d017b34d9785ca8edf514d2a46d88e533
Dagmar Suchá <78727813+dag-mara@users.noreply.github.com>
2023-02-07 21:51:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,26 @@
 
 /* tady bude tvůj kód */
 console.log('JavaScript ve stránce funguje');
+
+const movieList = document.querySelector('#movies');
+const showMovies = () =>{
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
+    .then((resp) => resp.json())
+    .then((data) => {
+        
+        data.forEach((movie) => {
+            const movieTemplate = `
+            <div class="movie">
+                <img class="movie__img" src="${movie.posterUrl}" alt="${movie.title}">
+                <a href="${movie.url}"><h2 class="movie__title">${movie.title}</h2></a>
+                <p class="movie__year">${movie.year}</p>
+                <p class="movie__genre">${movie.genres.join(', ')}</p>
+            </div>
+            ` 
+            movieList.innerHTML = movieTemplate;
+            console.log(movie);
+        });
+    });
+}
+
+showMovies();
